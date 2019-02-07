fee542e82be9387ce44799941a31225a0fc0285d
Albert Hu <al.hu@berkeley.edu>
2019-02-06 16:28:46 -0800

    change .of to .list

--- lab3/IntList/IntListTest.java
+++ lab3/IntList/IntListTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class IntListTest {
 
     /**
-     * Example test that verifies correctness of the IntList.list static
+     * Example test that verifies correctness of the IntList.of static
      * method. The main point of this is to convince you that
      * assertEquals knows how to handle IntLists just fine.
      */
@@ -16,25 +16,25 @@ public class IntListTest {
         IntList twoOne = new IntList(2, one);
         IntList threeTwoOne = new IntList(3, twoOne);
 
-        IntList x = IntList.list(3, 2, 1);
+        IntList x = IntList.of(3, 2, 1);
         assertEquals(threeTwoOne, x);
     }
 
     @Test
     public void testdSquareList() {
-        IntList L = IntList.list(1, 2, 3);
+        IntList L = IntList.of(1, 2, 3);
         IntList.dSquareList(L);
-        assertEquals(IntList.list(1, 4, 9), L);
+        assertEquals(IntList.of(1, 4, 9), L);
     }
 
     /**
      * Do not use the new keyword in your tests. You can create
-     * lists using the handy IntList.list method.
+     * lists using the handy IntList.of method.
      * <p>
      * Make sure to include test cases involving lists of various sizes
      * on both sides of the operation. That includes the empty list, which
      * can be instantiated, for example, with
-     * IntList empty = IntList.list().
+     * IntList empty = IntList.of().
      * <p>
      * Keep in mind that dcatenate(A, B) is NOT required to leave A untouched.
      * Anything can happen to A.
@@ -42,28 +42,28 @@ public class IntListTest {
 
     @Test
     public void testSquareListRecursive() {
-        IntList L = IntList.list(1, 2, 3);
+        IntList L = IntList.of(1, 2, 3);
         IntList res = IntList.squareListRecursive(L);
-        assertEquals(IntList.list(1, 2, 3), L);
-        assertEquals(IntList.list(1, 4, 9), res);
+        assertEquals(IntList.of(1, 2, 3), L);
+        assertEquals(IntList.of(1, 4, 9), res);
     }
 
     @Test
     public void testDcatenate() {
-        IntList A = IntList.list(1, 2, 3);
-        IntList B = IntList.list(4, 5, 6);
-        IntList exp = IntList.list(1, 2, 3, 4, 5, 6);
+        IntList A = IntList.of(1, 2, 3);
+        IntList B = IntList.of(4, 5, 6);
+        IntList exp = IntList.of(1, 2, 3, 4, 5, 6);
         assertEquals(exp, IntList.dcatenate(A, B));
-        assertEquals(IntList.list(1, 2, 3, 4, 5, 6), A);
+        assertEquals(IntList.of(1, 2, 3, 4, 5, 6), A);
     }
 
     @Test
     public void testCatenate() {
-        IntList A = IntList.list(1, 2, 3);
-        IntList B = IntList.list(4, 5, 6);
-        IntList exp = IntList.list(1, 2, 3, 4, 5, 6);
+        IntList A = IntList.of(1, 2, 3);
+        IntList B = IntList.of(4, 5, 6);
+        IntList exp = IntList.of(1, 2, 3, 4, 5, 6);
         assertEquals(exp, IntList.catenate(A, B));
-        assertEquals(IntList.list(1, 2, 3), A);
+        assertEquals(IntList.of(1, 2, 3), A);
     }
 
     /** If you're running this from the command line, you'll need
