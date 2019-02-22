5eaa98b7af3ecfc871ddbb60a14f4132a4b196fa
Christine Zhou <cazhou@berkeley.edu>
2019-02-22 12:46:25 -0800

    hw1: Release hw1.

---
+++ hw1/es/datastructur/synthesizer/ArrayRingBuffer.java
@@ -0,0 +1,59 @@
+package es.datastructur.synthesizer;
+import java.util.Iterator;
+
+//TODO: Make sure to that this class and all of its methods are public
+//TODO: Make sure to add the override tag for all overridden methods
+//TODO: Make sure to make this class implement BoundedQueue<T>
+
+public class ArrayRingBuffer<T>  {
+    /* Index for the next dequeue or peek. */
+    private int first;
+    /* Index for the next enqueue. */
+    private int last;
+    /* Variable for the fillCount. */
+    private int fillCount;
+    /* Array for storing the buffer data. */
+    private T[] rb;
+
+    /**
+     * Create a new ArrayRingBuffer with the given capacity.
+     */
+    public ArrayRingBuffer(int capacity) {
+        // TODO: Create new array with capacity elements.
+        //       first, last, and fillCount should all be set to 0.
+    }
+
+    /**
+     * Adds x to the end of the ring buffer. If there is no room, then
+     * throw new RuntimeException("Ring buffer overflow").
+     */
+    public void enqueue(T x) {
+        // TODO: Enqueue the item. Don't forget to increase fillCount and update
+        //       last.
+        return;
+    }
+
+    /**
+     * Dequeue oldest item in the ring buffer. If the buffer is empty, then
+     * throw new RuntimeException("Ring buffer underflow").
+     */
+    public T dequeue() {
+        // TODO: Dequeue the first item. Don't forget to decrease fillCount and
+        //       update first.
+        return null;
+    }
+
+    /**
+     * Return oldest item, but don't remove it. If the buffer is empty, then
+     * throw new RuntimeException("Ring buffer underflow").
+     */
+    public T peek() {
+        // TODO: Return the first item. None of your instance variables should
+        //       change.
+        return null;
+    }
+
+    // TODO: When you get to part 4, implement the needed code to support
+    //       iteration and equals.
+}
+    // TODO: Remove all comments that say TODO when you're done.
---
+++ hw1/es/datastructur/synthesizer/TestArrayRingBuffer.java
@@ -0,0 +1,14 @@
+package es.datastructur.synthesizer;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/** Tests the ArrayRingBuffer class.
+ *  @author Josh Hug
+ */
+
+public class TestArrayRingBuffer {
+    @Test
+    public void someTest() {
+        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+    }
+}
