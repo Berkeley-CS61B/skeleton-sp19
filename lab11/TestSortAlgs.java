a65b47fe681d86fb17efb0824c31ac136376b658
Joe Deatrick <jldeatrick@protonmail.com>
2019-04-08 03:49:03 -0700

    release lab 11

---
+++ lab11/TestSortAlgs.java
@@ -0,0 +1,38 @@
+import edu.princeton.cs.algs4.Queue;
+
+import org.junit.Test;
+
+public class TestSortAlgs {
+
+    @Test
+    public void testQuickSort() {
+
+    }
+
+    @Test
+    public void testMergeSort() {
+
+    }
+
+    /**
+     * Returns whether a Queue is sorted or not.
+     *
+     * @param items  A Queue of items
+     * @return       true/false - whether "items" is sorted
+     */
+    private <Item extends Comparable> boolean isSorted(Queue<Item> items) {
+        if (items.size() <= 1) {
+            return true;
+        }
+        Item curr = items.dequeue();
+        Item prev = curr;
+        while (!items.isEmpty()) {
+            prev = curr;
+            curr = items.dequeue();
+            if (curr.compareTo(prev) < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
